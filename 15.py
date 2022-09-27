0563b9066017abb77710d83d1f8d85d77f1668eb
Юлия Кобыличенко <113581457+yulliens@users.noreply.github.com>
2022-09-27 18:47:00 +0300

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,9 @@
+meal = input('Прием пищи:')
+if meal == 'Завтрак':
+    print('Каша')
+else:
+    meal = input('Предпочтение:')
+    if meal == 'Поесть плотно':
+        print('Плов')
+    else:
+        print('Котлета с пюре')
